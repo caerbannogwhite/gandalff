906b64a7d43d34737093eedb27bb329b259b799c
caerbannogwhite <massimo.meneghello93@gmail.com>
2023-10-27 23:43:10 +0100

    [io] Implementing Xpt reader version 8/9.

--- io_xpt.go
+++ io_xpt.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"os"
 	"runtime"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -130,27 +131,57 @@ func (w *XptWriter) SetDataFrame(dataframe DataFrame) *XptWriter {
 
 ///////////////////////////////////////     SAS XPT v5/6     ///////////////////////////////////////
 
+const (
+	valueHeader = "HEADER RECORD*******"
+	valueSAS    = "SAS     "
+	valueLIB    = "SASLIB  "
+)
+
 // Technical documentation:
 // https://support.sas.com/content/dam/SAS/support/en/technical-papers/record-layout-of-a-sas-version-5-or-6-data-set-in-sas-transport-xport-format.pdf
 const FIRST_HEADER_V56 = "HEADER RECORD*******LIBRARY HEADER RECORD!!!!!!!000000000000000000000000000000"
 
-// This functions writes a SAS XPT file (versions 5/6).
+// This functions reads a SAS XPT file (versions 5/6).
 func readXPTv56(reader io.Reader, ctx *Context) ([]string, []Series, error) {
 	if ctx == nil {
 		return nil, nil, fmt.Errorf("readCSV: no context specified")
 	}
 
 	var err error
-	var buff []byte
 
-	_, err = reader.Read(buff)
-	if err != nil {
+	content := make([]byte, 0)
+	buffer := make([]byte, 1024)
+	for n, err := reader.Read(buffer); err == nil; n, err = reader.Read(buffer) {
+		content = append(content, buffer[:n]...)
+	}
+
+	if err != nil && err != io.EOF {
 		return nil, nil, err
 	}
 
-	if string(buff[0:78]) != FIRST_HEADER_V56 {
-		return nil, nil, fmt.Errorf("readXPTv56: invalid header")
+	offset := 0
+
+	// check header
+	if string(content[0:20]) != valueHeader {
+		return nil, nil, fmt.Errorf("readXPV56: invalid header")
 	}
+	offset += 80
+
+	// check the first real header record
+	if string(content[offset:offset+8]) != valueSAS ||
+		string(content[offset+8:offset+16]) != valueSAS ||
+		string(content[offset+16:offset+24]) != valueLIB {
+		return nil, nil, fmt.Errorf("readXPV56: invalid first real header")
+	}
+
+	version := strings.Trim(string(content[offset+24:offset+32]), " ")
+	if version != "" { // TODO: check version
+		return nil, nil, fmt.Errorf("readXPTv56: invalid version '%s'", version)
+	}
+	offset += 80
+
+	// check the second real header record
+	offset += 80
 
 	return nil, nil, nil
 }
@@ -177,44 +208,95 @@ func writeXPTv56(path string) error {
 // Technical documentation:
 // https://support.sas.com/content/dam/SAS/support/en/technical-papers/record-layout-of-a-sas-version-8-or-9-data-set-in-sas-transport-format.pdf
 
-const FIRST_HEADER_V89 = "HEADER RECORD*******LIBV8 HEADER RECORD!!!!!!!000000000000000000000000000000"
-
-// This functions writes a SAS XPT file (versions 7/8).
+// This functions reads a SAS XPT file (versions 8/9).
 func readXPTv89(reader io.Reader, ctx *Context) ([]string, []Series, error) {
 	if ctx == nil {
 		return nil, nil, fmt.Errorf("readXPTv89: no context specified")
 	}
 
 	var err error
-	var buff []byte
 
-	_, err = reader.Read(buff)
-	if err != nil {
+	content := make([]byte, 0)
+	buffer := make([]byte, 1024)
+	for n, err := reader.Read(buffer); err == nil; n, err = reader.Read(buffer) {
+		content = append(content, buffer[:n]...)
+	}
+
+	if err != nil && err != io.EOF {
 		return nil, nil, err
 	}
 
-	// check header
-	if string(buff[0:76]) != FIRST_HEADER_V89 {
+	offset := 0
+
+	///////////////////////////////////////
+	// 1	The first header record consists ofthe following characterstring, in ASCII:
+	// 		HEADER RECORD*******LIBV8 HEADER RECORD!!!!!!!000000000000000000000000000000
+	if string(content[0:20]) != valueHeader {
 		return nil, nil, fmt.Errorf("readXPTv89: invalid header")
 	}
+	offset += 80
 
-	buff = buff[76:]
-
-	// check real header
-	if strings.Trim(string(buff[:8]), " ") != "SAS" || strings.Trim(string(buff[8:16]), " ") != "SAS" {
-		return nil, nil, fmt.Errorf("readXPTv89: invalid header")
+	///////////////////////////////////////
+	// 2	The first real header record
+	if string(content[offset:offset+8]) != valueSAS ||
+		string(content[offset+8:offset+16]) != valueSAS ||
+		string(content[offset+16:offset+24]) != valueLIB {
+		return nil, nil, fmt.Errorf("readXPTv89: invalid first real header")
 	}
-	if strings.Trim(string(buff[16:24]), " ") != "SASLIB  9.4" {
-		return nil, nil, fmt.Errorf("readXPTv89: invalid version")
+
+	version := strings.Trim(string(content[offset+24:offset+32]), " ")
+	if version != "9.4" {
+		return nil, nil, fmt.Errorf("readXPTv89: invalid version '%s'", version)
+	}
+	offset += 80
+
+	///////////////////////////////////////
+	// 3	Second real header record: ddMMMyy:hh:mm:ss
+	offset += 80
+
+	///////////////////////////////////////
+	// 4	Member header records
+	if string(content[offset:offset+20]) != valueHeader {
+		return nil, nil, fmt.Errorf("readXPTv89: invalid member header")
 	}
 
-	buff = buff[80:]
+	namestrSize, err := strconv.Atoi(string(content[offset+74 : offset+78]))
+	if err != nil {
+		return nil, nil, fmt.Errorf("readXPTv89: invalid NAMESTR size '%s'", string(content[offset+74:offset+78]))
+	}
+	offset += 80
+
+	switch namestrSize {
+	case 140:
+		// TODO: read namestr
+	default:
+		return nil, nil, fmt.Errorf("readXPTv89: invalid NAMESTR size '%d'", namestrSize)
+	}
+
+	// skip the member header
+	offset += 80
+
+	///////////////////////////////////////
+	// 5	Member header data
+
+	// skip the member header data
+	offset += 80
+	// skip the header record
+	offset += 80
+
+	///////////////////////////////////////
+	// 6	Namestr headerrecord
+
+	///////////////////////////////////////
+	// 7	Namestr records
 
 	return nil, nil, nil
 }
 
-// This functions writes a SAS XPT file (versions 7/8).
+// This functions writes a SAS XPT file (versions 8/9).
 func writeXPTv89(path string) error {
+	const firstHeaderRecord = "HEADER RECORD*******LIBRARY HEADER RECORD!!!!!!!000000000000000000000000000000  "
+
 	var osVersion string
 	switch runtime.GOOS {
 	case "darwin":
@@ -230,13 +312,14 @@ func writeXPTv89(path string) error {
 	buff := make([]byte, 0)
 
 	buff = append(buff, []byte(fmt.Sprintf(
-		"%s%8s%8s%8s%8s%24s%16s%80s",
-		FIRST_HEADER_V89,                      // 1-80 		First header record
-		"SAS",                                 // 81-88 	SAS
-		"SAS",                                 // 89-96 	SAS
-		"SASLIB  9.4",                         // 97-104 	SASLIB
-		osVersion,                             // 105-128	OS Name
-		"",                                    // 129-152 	24 blanks
+		"%s%8s%8s%8s%8s%8s%24s%16s%80s",
+		firstHeaderRecord,                     // 1-80 		First header record
+		"SAS     ",                            // 81-88 	SAS
+		"SAS     ",                            // 89-96 	SAS
+		"SASLIB  ",                            // 97-104 	SASLIB
+		"9.4     ",                            // 105-112 	SAS Version
+		osVersion,                             // 113-120 	Operating System
+		"",                                    //
 		time.Now().Format("ddMMMyy:hh:mm:ss"), // 153-176   Date/time created
 		time.Now().Format("ddMMMyy:hh:mm:ss"), // 177-200 	Second header record, date/time modified
 	))...)
